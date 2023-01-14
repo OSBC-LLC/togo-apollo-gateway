2cb82ed1def0aa8d453b7082264eaa0e535ee53f
William Vandebelt <will@Williams-Mac-mini.local>
2023-01-13 21:32:22 -0500

    removed the timeout code

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const { ApolloServer } = require('apollo-server');
-const { ApolloGateway, IntrospectAndCompose, RemoteGraphQLDataSource } = require('@apollo/gateway');
+const { ApolloGateway, IntrospectAndCompose } = require('@apollo/gateway');
 const depthLimit = require('graphql-depth-limit');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
-let timeoutStr = process.env.TIMEOUT || 3000;
-const timeout = parseInt(timeoutStr, 10);
 let depthLimitStr = process.env.DEPTH_LIMIT || 3;
 const depth = parseInt(depthLimitStr, 10);
 
@@ -18,18 +16,6 @@ const gateway = new ApolloGateway({
 			{name: "user", url: process.env.SUBGRAPH_ONE},
 		]
 	}),
-	buildService({ name, url }) {
-		// Sets a 3 second timeout on requests
-		const fetcher = (input, init) => {
-			if (init) {
-				init.timeout = timeout;
-			} else {
-				init = { timeout: timeout};
-			}
-			return fetch(input, init);
-		};
-		return new RemoteGraphQLDataSource({ url, fetcher })
-	}
 });
 
 const server = new ApolloServer({
